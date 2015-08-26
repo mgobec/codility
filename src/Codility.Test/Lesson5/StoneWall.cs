b36afcbba2005cdf19becab246cbabc691889f2e
mgobec <matija0204@gmail.com>
2015-08-26 14:55:52 +0200

    CountDiv & StoneWall

---
+++ src/Codility.Test/Lesson5/StoneWall.cs
@@ -0,0 +1,20 @@
+﻿namespace Codility.Test.Lesson5
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using main = Codility.Lesson5;
+
+    [TestClass]
+    public class StoneWall
+    {
+        private main.StoneWall stoneWall = new main.StoneWall();
+
+        [TestMethod]
+        public void SimpleTest()
+        {
+            var array = new int[] { 8, 8, 5, 7, 9, 8, 7, 4, 8 };
+            var result = stoneWall.Solution(array);
+
+            Assert.AreEqual(7, result);
+        }
+    }
+}
---
+++ src/Codility/Lesson3/CountDiv.cs
@@ -0,0 +1,37 @@
+﻿namespace Codility.Lesson3
+{
+    /// <summary>
+    /// Write a function:
+    /// 
+    /// class Solution { public int solution(int A, int B, int K); }
+    /// 
+    ///     that, given three integers A, B and K, returns the number of integers within the range[A..B] that are divisible by K, i.e.:
+    /// 
+    /// { i : A ≤ i ≤ B, i mod K = 0 }
+    /// 
+    /// For example, for A = 6, B = 11 and K = 2, your function should return 3, because there are three numbers divisible by 2 within the range[6..11], namely 6, 8 and 10.
+    /// 
+    /// Assume that:
+    /// 
+    /// A and B are integers within the range[0..2, 000, 000, 000];
+    /// K is an integer within the range[1..2, 000, 000, 000];
+    /// A ≤ B.
+    /// Complexity:
+    /// 
+    /// expected worst-case time complexity is O(1);
+    /// expected worst-case space complexity is O(1).
+    /// Copyright 2009–2015 by Codility Limited.All Rights Reserved.Unauthorized copying, publication or disclosure prohibited.
+    /// </summary>
+    public class CountDiv
+    {
+        public int Solution(int A, int B, int K)
+        {
+            var start = (A / K) * K;
+            var count = (B - start) / K;
+
+            if (A % K == 0) count++;
+
+            return count;
+        }
+    }
+}
